56a987105559fd1516962091150ccf68071b0341
smaugerfr <samuel@smauger.fr>
2022-10-30 20:35:37 +0100

    Add seed param random class

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ class Main {
      * @param args
      */
     public static void main(String[] args) {
-        Tree a = Tree.generateRandomtree(400, 0.3, 5, 0.5, 2, new Random(), 1400, 1000);
+        Tree a = Tree.generateRandomtree(400, 0.3, 5, 0.5, 2, new Random(2), 1400, 1000);
         Image img=a.toImage();
         
         try {
--- Random.java
+++ Random.java
@@ -3,4 +3,8 @@ public class Random extends java.util.Random {
     Random(){
         super(1);
     }
+
+    Random(int seed){
+        super(seed);
+    }
 }
