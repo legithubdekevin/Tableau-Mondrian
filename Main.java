d2561f648a091805e49bc2eb6af795094981b8b1
Kevin <kevin.misrahi@gmail.com>
2022-10-30 17:45:06 +0100

    Push Debug Main Tree

--- Main.java
+++ Main.java
@@ -4,7 +4,15 @@ class Main {
      * @param args
      */
     public static void main(String[] args) {
-        Tree a = Tree.generateRandomtree(10, 0.45, 50, 0.4, 5, new Random(), 1000, 1000);
+        Tree a = Tree.generateRandomtree(10, 0.45, 5, 0.1, 2, new Random(), 1000, 1000);
+        Image img=a.toImage();
         
+        try {
+            img.save("img.png");
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+
     }
 }
