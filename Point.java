a5f65b81f412cd37d4489c59902b882d6292c92e
smaugerfr <samuel@smauger.fr>
2022-10-30 12:05:03 +0100

    Add comments

--- Point.java
+++ Point.java
@@ -1,23 +1,40 @@
 public class Point {
     private int x,y;
 
+    /**
+     * Contructor Point
+     * @param x
+     * @param y
+     */
     Point(int x, int y){
         this.x=x;
         this.y=y;
     }
 
-
+    /**
+     * Getter X
+     */
     public int getx(){
         return this.x;
     }
 
+    /**
+     * Getter Y
+     */
     public int gety(){
         return this.y;
     }
 
+    /**
+     * Setter X
+     */
     public void setx(int x){
         this.x=x;
     }
+
+    /**
+     * Setter Y
+     */
     public void sety(int y){
         this.y=y;
     }
