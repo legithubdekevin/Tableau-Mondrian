886e240e72092e41e35c0a314d2a2c9dc19e56b8
Kevin <kevin.misrahi@gmail.com>
2022-10-30 10:54:26 +0100

    Create Point Class

---
+++ Point.java
@@ -0,0 +1,25 @@
+public class Point {
+    private int x,y;
+
+    Point(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+
+
+    public int getx(){
+        return this.x;
+    }
+
+    public int gety(){
+        return this.y;
+    }
+
+    public void setx(int x){
+        this.x=x;
+    }
+    public void sety(int y){
+        this.y=y;
+    }
+
+} 
