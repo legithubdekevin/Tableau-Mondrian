48789c82cdb2e50bdfa043c7da395821dcecd6ca
smaugerfr <samuel@smauger.fr>
2022-10-30 14:07:30 +0100

    Add main

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ class Main {
      * @param args
      */
     public static void main(String[] args) {
+        Tree a = Tree.generateRandomtree(10, 0.45, 50, 0.4, 5, new Random(), 1000, 1000);
         
     }
 }
