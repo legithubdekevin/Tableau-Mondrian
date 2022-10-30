55d7414f633e44de4d21f479788c7d325601674c
Kevin <kevin.misrahi@gmail.com>
2022-10-30 11:26:24 +0100

    Update

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        Random generat = new Random();
+        double rand1 = generat.nextDouble();
+        double rand2 = generat.nextDouble();
+        System.out.println(rand1 + "+" + rand2); 
     }
 }
