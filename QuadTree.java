dd9ced8841c2a85676bbc40aad5fe9c6f463ea1c
David Guetta <johndoe@example.com>
2022-11-18 10:46:06 +0100

    Create method addPoint

--- QuadTree.java
+++ QuadTree.java
@@ -10,6 +10,14 @@ public class QuadTree {
         this.v4 = null;
     }
 
+    QuadTree(Tree coord, QuadTree v1, QuadTree v2, QuadTree v3, QuadTree v4){
+        this.coord=coord;
+        this.v1 = v1;
+        this.v2 = v2;
+        this.v3 = v3;
+        this.v4 = v4;
+    }
+
     public Point getCoord() {
         return coord.getCoord();
     }
@@ -29,6 +37,24 @@ public class QuadTree {
         }
         else{
             return a.coord;
+        }
+    }
+
+    public static QuadTree addPoint(Tree coord, QuadTree a){
+        if(coord.getCoord().getx()<a.getCoord().getx()&& coord.getCoord().gety()<a.getCoord().gety()&& a.v1!=null){
+            return new QuadTree(a.coord, addPoint(coord, a.v1), a.v2, a.v3, a.v4);
+        }
+        else if(coord.getCoord().getx()>=a.getCoord().getx()&&coord.getCoord().gety()<a.getCoord().gety()&& a.v2!=null){
+            return new QuadTree(a.coord, a.v1, addPoint(coord, a.v2), a.v3, a.v4);
+        }
+        else if(coord.getCoord().getx()>=a.getCoord().getx()&&coord.getCoord().gety()>=a.getCoord().gety()&& a.v3!=null){
+            return new QuadTree(a.coord, a.v1, a.v2, addPoint(coord, a.v3), a.v4);
+        }
+        else if(coord.getCoord().getx()<a.getCoord().getx()&&coord.getCoord().gety()>=a.getCoord().gety()&& a.v4!=null){
+            return new QuadTree(a.coord, a.v1, a.v2, a.v3, addPoint(coord, a.v4));
+        }
+        else{
+            return new QuadTree(coord);
         }
     }
 
