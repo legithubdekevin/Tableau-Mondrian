cfc0ebc3aa45d7c19040e050cde9e84189905f40
David Guetta <johndoe@example.com>
2022-11-22 09:17:13 +0100

    TOFIX strategy

--- Tree.java
+++ Tree.java
@@ -281,29 +281,90 @@ public class Tree {
         Tree a = new Tree(width, height, rnd);
         QuadTree pp = new QuadTree(a);
         int leaves = 1;
-        for(int i = 0; i<nbLeaves ; i++){
+        boolean isPossible = true;
+        while(leaves < nbLeaves && isPossible){
             //Choose division Axis
             boolean isDivisionAxisX = rnd.nextDouble() < proportionCut;
             //Generate random point interpolate
             Point rndPoint;
             if(isDivisionAxisX){
-                int startX = rnd.nextDouble() < (center.getx() / width) ? 0 : width;
-                int interpolateX = startX+(center.getx() - startX)*(leaves/nbLeaves);
+                //Find max interval to divide
+                int startX = (center.getx()) < (width - center.getx()) ? width : 0;
+                //Find interpol point
+                int interpolateX = startX+(int)((center.getx() - startX)*((double)leaves/(double)nbLeaves));
+                //Generate random y
                 int randomY = (int)(rnd.nextDouble() * height);
                 rndPoint = new Point(interpolateX, randomY);
             }else{
-                int startY = rnd.nextDouble() < (center.gety() / height) ? 0 : height;
-                int interpolateY = startY+(center.gety() - startY)*(leaves/nbLeaves); 
+                //Find max interval to divide
+                int startY = (center.gety()) < (height - center.gety()) ? height : 0;
+                //Find interpol point
+                int interpolateY = startY+(int)((center.gety() - startY)*((double)leaves/(double)nbLeaves));
+                //Generate random x
                 int randomX = (int)(rnd.nextDouble() * width);
                 rndPoint = new Point(randomX, interpolateY);
             }
-
+            //System.out.println(rndPoint.getx()+", "+rndPoint.gety());
             //Trouver la feuille contenant le point
-            //Tree leaf = QuadTree.getTreeContained(rndPoint, pp);
-            System.out.println(rndPoint.gety()+", "+ rndPoint.gety());
-            //Bref
+            Tree leaf = QuadTree.getTreeContained(rndPoint, pp);
+            if(isDivisionPossible(leaf, minDimensionCut, isDivisionAxisX)){
+                System.out.println("LEAF "+leaf.coord.getx()+", "+leaf.coord.gety()+" h: "+leaf.height+" w: "+leaf.width);
+                leaf.setDivisionAxis(isDivisionAxisX);
 
-            leaves++;
+                
+
+                Point leftCoord;
+                Point rightCoord;
+                int leftWidth;
+                int rightWidth;
+                int leftHeight;
+                int rightHeight;
+
+                if(isDivisionAxisX){
+                    //Case X division
+                    leftCoord   = leaf.coord;
+                    rightCoord  = new Point(rndPoint.getx()+(widthLine/2), leaf.coord.gety());
+                    //Case the width line is impair we slice left part
+                    leftWidth   = rndPoint.getx() - leftCoord.getx() - (int)Math.ceil((double)widthLine/2);
+                    rightWidth  = leaf.coord.getx()+leaf.width - rndPoint.getx() - (int)Math.floor((double)widthLine/2);
+                    leftHeight  = leaf.height;
+                    rightHeight = leaf.height;
+                    System.out.println("startL : "+leftCoord.getx()+", "+leftCoord.gety());
+                    System.out.println("endL : "+(leftCoord.getx()+leftWidth)+", "+(leftCoord.gety()+leftHeight));
+                    System.out.println("startR : "+rightCoord.getx()+", "+rightCoord.gety());
+                    System.out.println("endR : "+(rightCoord.getx()+rightWidth)+", "+(rightCoord.gety()+rightHeight));
+
+                }else{
+                    //Case Y division
+                    leftCoord   = leaf.coord;
+                    rightCoord  = new Point(leaf.coord.getx(), rndPoint.gety()+(widthLine/2));
+                    leftWidth   = leaf.width;
+                    rightWidth  = leaf.width;
+                    //Case the width line is impair we slice left part 
+                    leftHeight  = rndPoint.gety() - leaf.coord.gety() - (int)Math.ceil((double)widthLine/2);
+                    rightHeight = leaf.coord.gety()+leaf.height - rndPoint.gety() - (int)Math.floor((double)widthLine/2);
+                    System.out.println("startL : "+leftCoord.getx()+", "+leftCoord.gety());
+                    System.out.println("endL : "+(leftCoord.getx()+leftWidth)+", "+(leftCoord.gety()+leftHeight));
+                    System.out.println("startR : "+rightCoord.getx()+", "+rightCoord.gety());
+                    System.out.println("endR : "+(rightCoord.getx()+rightWidth)+", "+(rightCoord.gety()+rightHeight));
+                }
+                
+                //Create child
+                leaf.left = new Tree(leftCoord, leftWidth, leftHeight, rnd);
+                leaf.left.chooseColor(leaf.color, sameColorProb);
+                leaf.right = new Tree(rightCoord, rightWidth, rightHeight, rnd);
+                leaf.right.chooseColor(leaf.color, sameColorProb);
+                //insert child in QuadTree
+                pp = QuadTree.addPoint(leaf.right, pp);
+                
+                leaves+=1 ;
+                // Image img=a.toImage();
+                // try{
+                //     img.save("img"+leaves+".png");
+                // }
+            }else{
+                isPossible = false;
+            }
         }
 
         return a;
