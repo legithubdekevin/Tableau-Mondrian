7f2385525dd568230bf134ffb05b125fbe38e3e1
smaugerfr <samuel@smauger.fr>
2022-11-24 14:30:01 +0100

    Fix addPoint and add draw function

--- QuadTree.java
+++ QuadTree.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+
 public class QuadTree {
     QuadTree v1,v2,v3,v4;
     Tree coord;
@@ -41,20 +43,78 @@ public class QuadTree {
     }
 
     public static QuadTree addPoint(Tree coord, QuadTree a){
-        if(coord.getCoord().getx()<a.getCoord().getx()&& coord.getCoord().gety()<a.getCoord().gety()&& a.v1!=null){
-            return new QuadTree(a.coord, addPoint(coord, a.v1), a.v2, a.v3, a.v4);
+        if(coord.getCoord().getx()<a.getCoord().getx()){
+            //Partie gauche
+            if(coord.getCoord().gety()<a.getCoord().gety()){
+                //Partie haute
+                //V1
+                System.out.println("v1");
+                if(a.v1 == null){
+                    return new QuadTree(a.coord, new QuadTree(coord), a.v2, a.v3, a.v4);
+                }else{
+                    return new QuadTree(a.coord, addPoint(coord, a.v1), a.v2, a.v3, a.v4);
+                }
+            }else{
+                //Partie Basse
+                //V4
+                System.out.println("v4");
+                if(a.v4 == null){
+                    return new QuadTree(a.coord, a.v1, a.v2, a.v3, new QuadTree(coord));
+                }else{
+                    return new QuadTree(a.coord, a.v1, a.v2, a.v3, addPoint(coord, a.v4));
+                }
+
+            }
+        }else{
+             //Partie droite
+             if(coord.getCoord().gety()<a.getCoord().gety()){
+                //Partie haute
+                //V2
+                System.out.println("v2");
+                if(a.v2 == null){
+                    return new QuadTree(a.coord, a.v1, new QuadTree(coord), a.v3, a.v4);
+                }else{
+                    return new QuadTree(a.coord, a.v1, addPoint(coord, a.v2), a.v3, a.v4);
+                }
+
+            }else{
+                //Partie Basse
+                //V3
+                System.out.println("v3");
+                if(a.v3 == null){
+                    return new QuadTree(a.coord, a.v1, a.v2, new QuadTree(coord), a.v4);
+                }else{
+                    return new QuadTree(a.coord, a.v1, a.v2, addPoint(coord, a.v3), a.v4);
+                }
+            }
         }
-        else if(coord.getCoord().getx()>=a.getCoord().getx()&&coord.getCoord().gety()<a.getCoord().gety()&& a.v2!=null){
-            return new QuadTree(a.coord, a.v1, addPoint(coord, a.v2), a.v3, a.v4);
-        }
-        else if(coord.getCoord().getx()>=a.getCoord().getx()&&coord.getCoord().gety()>=a.getCoord().gety()&& a.v3!=null){
-            return new QuadTree(a.coord, a.v1, a.v2, addPoint(coord, a.v3), a.v4);
-        }
-        else if(coord.getCoord().getx()<a.getCoord().getx()&&coord.getCoord().gety()>=a.getCoord().gety()&& a.v4!=null){
-            return new QuadTree(a.coord, a.v1, a.v2, a.v3, addPoint(coord, a.v4));
-        }
-        else{
-            return new QuadTree(coord);
+    }
+
+    public static Image draw(QuadTree tree, int width, int height){
+        Image img = new Image(width, height);
+        img.setRectangle(0, width, 0, height, Color.red);
+        drawRec(tree, img, 0, 0, width, height);
+        return img;
+    }
+
+    public static void drawRec(QuadTree tree, Image img, int startX, int startY, int width, int height){
+        if(tree != null){
+            System.out.println(startX+", "+startY);
+            System.out.println(width+"x"+height);
+            System.out.println(tree.getCoord().getx()+", "+tree.getCoord().gety());
+            //Draw horizontal
+            img.setRectangle(startX, width, tree.coord.getCoord().gety()+2, tree.coord.getCoord().gety()-2, Color.white);
+            //Draw vertical
+            img.setRectangle(tree.coord.getCoord().getx()+2, tree.coord.getCoord().getx()-2, startY, height, Color.white);
+            System.out.println(tree.v1);
+            System.out.println(tree.v2);
+            System.out.println(tree.v3);
+            System.out.println(tree.v4);
+            //Recall
+            drawRec(tree.v1, img, startX, startY, tree.coord.getCoord().getx()-startX, tree.coord.getCoord().gety()-startY);
+            drawRec(tree.v2, img, tree.coord.getCoord().getx(), startY, width-(tree.coord.getCoord().getx()-startX), (tree.coord.getCoord().gety()-startY));
+            drawRec(tree.v3, img, tree.coord.getCoord().getx(), tree.coord.getCoord().gety(), width-(tree.coord.getCoord().getx()-startX), height-(tree.coord.getCoord().gety()-startY));
+            drawRec(tree.v4, img, startX, tree.coord.getCoord().gety(), tree.coord.getCoord().getx()-startX, height-(tree.coord.getCoord().gety()-startY));
         }
     }
 
