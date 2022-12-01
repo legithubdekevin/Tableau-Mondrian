adae57fac5a72ea930514c267adcd70b95e5d227
smaugerfr <samuel@smauger.fr>
2022-12-01 18:57:01 +0100

    Add args control

--- Main.java
+++ Main.java
@@ -3,64 +3,14 @@ class Main {
     static final int RANDOM_SEED = 0;
     static final int WIDTH = 500;
     static final int HEIGHT = 500;
-    private int nbLeaves;
-    private double proportionCut;
-    private int minDimensionCut;
-    private double sameColorProb;
-    private int widthLine;
-    private int centerX;
-    private int centerY;
-    private int choosedStrategy;
-
-    public void setnbLeaves(int val) throws Exception{
-        if(val < 1){
-            throw new Exception("Invalid nbLeaves value")
-        }
-        this.nbLeaves = val;
-    }
-
-    public void setProportionCut(double val) throws Exception{
-        if(val < 0 || val > 1){
-            throw new Exception("Invalid proportionCut value")
-        }
-        this.proportionCut = val;
-    }
-
-
-    public void setMinDimensionCut(int val) throws Exception{
-        if(val < 0){
-            throw new Exception("Invalid minDimensionCut value")
-        }
-        this.minDimensionCut = val;
-    }
-
-    public void setSameColorProb(double val) throws Exception{
-        if(val < 0 || val > 1){
-            throw new Exception("Invalid sameColorProb value")
-        }
-        this.sameColorProb = val;
-    }
-
-    public void setWidthLine(int val) throws Exception{
-        if(val < 0){
-            throw new Exception("Invalid widthLine value")
-        }
-        this.widthLine = val;
-    }
-
-    public void setCenterX(int val) throws Exception{
-        if(val < 0 || val > WIDTH){
-            throw new Exception("Invalid centerX value")
-        }
-        this.centerX = val;
-    }
-
-    public void setCenterY(int val) throws Exception{
-        if(val < 0 || val > HEIGHT){
-            throw new Exception("Invalid centerY value")
-        }
-        this.centerY = val;
-    }
+    static int nbLeaves;
+    static double proportionCut;
+    static int minDimensionCut;
+    static double sameColorProb;
+    static int widthLine;
+    static int centerX;
+    static int centerY;
+    static int choosedStrategy;
 
     /**
      * 
@@ -68,33 +18,116 @@ class Main {
      */
     public static void main(String[] args) {
 
-        for(String arg : args){
-            System.out.println(arg);
-        }
         try{
-            this.setnbLeaves(Integer.parseInt(args[0]));
-            this.setProportionCut(Double.parseDouble(args[1]));
-            this.setMinDimensionCut(Integer.parseInt(args[2]));
-            this.setSameColorProb(Double.parseDouble(args[3]));
-            this.widthLine(Integer.parseInt(args[4]));
-            this.setCenterX(Integer.parseInt(args[5]));
-            this.setCenterY(Integer.parseInt(args[6]));
-            this.choosedStrategy = Integer.parseInt(args[7]);
+            if(args.length < 5) throw new Exception("Missing parameters");
+            setnbLeaves(Integer.parseInt(args[0]));
+            setProportionCut(Double.parseDouble(args[1]));
+            setMinDimensionCut(Integer.parseInt(args[2]));
+            setSameColorProb(Double.parseDouble(args[3]));
+            setWidthLine(Integer.parseInt(args[4]));
+            if(args.length > 6) {setCenterX(Integer.parseInt(args[5]));}
+            if(args.length > 7) {setCenterY(Integer.parseInt(args[6]));}
+            if(args.length > 8) {setChoosedStrategy(args[7]);}else{setChoosedStrategy(null);}
         }catch(Exception e){
             e.printStackTrace();
         }
 
         Random rnd = new Random(RANDOM_SEED);
-        Tree a = Tree.generateRandomtree(300, 0.5, 0, 0.5, 0, rnd, WIDTH, HEIGHT);
-        Image img=a.toImage();
-        
-        try {
-            img.save("img.png");
-        } catch (Exception e) {
-            // TODO: handle exception
-            e.printStackTrace();
+        Tree a;
+        Image img;
+
+        if(choosedStrategy == 0){
+            a = Tree.generateRandomtree(nbLeaves, proportionCut, minDimensionCut, sameColorProb, widthLine, rnd, WIDTH, HEIGHT);
+            img=a.toImage();
+
+            try {
+                img.save("img.png");
+            } catch (Exception e) {
+                // handle exception
+                e.printStackTrace();
+            }
+        }else if(choosedStrategy == 1){
+            a = Tree.generateBetterRandomtree(nbLeaves, proportionCut, minDimensionCut, sameColorProb, widthLine, rnd, WIDTH, HEIGHT, new Point(centerX, centerY));
+            img=a.toImage();
+
+            try {
+                img.save("img1.png");
+            } catch (Exception e) {
+                // handle exception
+                e.printStackTrace();
+            }
+        }else if(choosedStrategy == 2){
+            a = Tree.generateBetterRandomtree2(nbLeaves, proportionCut, minDimensionCut, sameColorProb, widthLine, rnd, WIDTH, HEIGHT, new Point(centerX, centerY));
+            img=a.toImage();
+
+            try {
+                img.save("img2.png");
+            } catch (Exception e) {
+                // handle exception
+                e.printStackTrace();
+            }
         }
+        
+        
 
 
+    }
+
+    public static void setnbLeaves(int val) throws Exception{
+        if(val < 1){
+            throw new Exception("Invalid nbLeaves value");
+        }
+        nbLeaves = val;
+    }
+
+    public static void setProportionCut(double val) throws Exception{
+        if(val < 0 || val > 1){
+            throw new Exception("Invalid proportionCut value");
+        }
+        proportionCut = val;
+    }
+
+
+    public static void setMinDimensionCut(int val) throws Exception{
+        if(val < 0){
+            throw new Exception("Invalid minDimensionCut value");
+        }
+        minDimensionCut = val;
+    }
+
+    public static void setSameColorProb(double val) throws Exception{
+        if(val < 0 || val > 1){
+            throw new Exception("Invalid sameColorProb value");
+        }
+        sameColorProb = val;
+    }
+
+    public static void setWidthLine(int val) throws Exception{
+        if(val < 0){
+            throw new Exception("Invalid widthLine value");
+        }
+        widthLine = val;
+    }
+
+    public static void setCenterX(int val) throws Exception{
+        if(val < 0 || val > WIDTH){
+            throw new Exception("Invalid centerX value");
+        }
+        centerX = val;
+    }
+
+    public static void setCenterY(int val) throws Exception{
+        if(val < 0 || val > HEIGHT){
+            throw new Exception("Invalid centerY value");
+        }
+        centerY = val;
+    }
+
+    public static void setChoosedStrategy(String val) throws Exception{
+        if(val == null){
+            choosedStrategy = 0;
+        }else{
+            choosedStrategy = Integer.parseInt(val);
+        }
     }
 }
