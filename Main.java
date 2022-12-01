abf80cfe392a2a22597f00c3eb33a9ac8481978a
Samuel MAUGER <samuel.mauger@etu.univ-nantes.fr>
2022-12-01 12:59:32 +0100

    Delete debug comments

--- Main.java
+++ Main.java
@@ -7,22 +7,11 @@ class Main {
         Tree a = Tree.generateBetterRandomtree2(300, 0.5, 0, 0.5, 0, new Random(3), 1000, 1000, new Point(500, 500));
         Image img=a.toImage();
         
-        // AVL Arbre = new AVL(0, null, null);
-        // System.out.println(Arbre);    
-        // Arbre = AVL.add(Arbre,-1).first();
-        // Arbre = AVL.add(Arbre,3).first();
-        // Arbre = AVL.add(Arbre,2).first();
-        // Arbre = AVL.add(Arbre,1).first();
-        // Arbre = AVL.add(Arbre,5).first();
-        // AVL.PrintAVL(Arbre);
-
-
-
-
         try {
             img.save("img.png");
         } catch (Exception e) {
             // TODO: handle exception
+            e.printStackTrace();
         }
 
 
