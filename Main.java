3f0d7b2cc18d9913e8c6f9560da1dbafdf21b2ca
Mona Lisa <Mona@Lisa.fr>
2022-12-02 11:39:31 +0100

    Add test avl

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 class Main {
 
     static final int RANDOM_SEED = 0;
-    static final int WIDTH = 500;
-    static final int HEIGHT = 500;
+    static final int WIDTH = 1400;
+    static final int HEIGHT = 1000;
     static int nbLeaves;
     static double proportionCut;
     static int minDimensionCut;
@@ -17,62 +17,61 @@ class Main {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println(args.length);
-        try{
-            if(args.length < 5) throw new Exception("Missing parameters");
-            setnbLeaves(Integer.parseInt(args[0]));
-            setProportionCut(Double.parseDouble(args[1]));
-            setMinDimensionCut(Integer.parseInt(args[2]));
-            setSameColorProb(Double.parseDouble(args[3]));
-            setWidthLine(Integer.parseInt(args[4]));
-            if(args.length > 6) {setCenterX(Integer.parseInt(args[5]));}
-            if(args.length > 7) {setCenterY(Integer.parseInt(args[6]));}
-            if(args.length >= 8) {setChoosedStrategy(args[7]);}else{setChoosedStrategy(null);}
-        }catch(Exception e){
-            e.printStackTrace();
+        if(args.length == 0){
+            AVL test = new AVL(null, null, null)
+        }else{
+            try{
+                if(args.length < 5) throw new Exception("Missing parameters");
+                setnbLeaves(Integer.parseInt(args[0]));
+                setProportionCut(Double.parseDouble(args[1]));
+                setMinDimensionCut(Integer.parseInt(args[2]));
+                setSameColorProb(Double.parseDouble(args[3]));
+                setWidthLine(Integer.parseInt(args[4]));
+                if(args.length > 6) {setCenterX(Integer.parseInt(args[5]));}
+                if(args.length > 7) {setCenterY(Integer.parseInt(args[6]));}
+                if(args.length >= 8) {setChoosedStrategy(args[7]);}else{setChoosedStrategy(null);}
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+    
+            Random rnd = new Random(RANDOM_SEED);
+            Tree a;
+            Image img;
+    
+            if(choosedStrategy == 0){
+                a = Tree.generateRandomtree(nbLeaves, proportionCut, minDimensionCut, sameColorProb, widthLine, rnd, WIDTH, HEIGHT);
+                img=a.toImage();
+    
+                try {
+                    img.save("img.png");
+                } catch (Exception e) {
+                    // handle exception
+                    e.printStackTrace();
+                }
+            }else if(choosedStrategy == 1){
+                System.out.println("test");
+                a = Tree.generateBetterRandomtree(nbLeaves, proportionCut, minDimensionCut, sameColorProb, widthLine, rnd, WIDTH, HEIGHT, new Point(centerX, centerY));
+                img=a.toImage();
+    
+                try {
+                    img.save("img1.png");
+                } catch (Exception e) {
+                    // handle exception
+                    e.printStackTrace();
+                }
+            }else if(choosedStrategy == 2){
+    
+                a = Tree.generateBetterRandomtree2(nbLeaves, proportionCut, minDimensionCut, sameColorProb, widthLine, rnd, WIDTH, HEIGHT, new Point(centerX, centerY));
+                img=a.toImage();
+    
+                try {
+                    img.save("img2.png");
+                } catch (Exception e) {
+                    // handle exception
+                    e.printStackTrace();
+                }
+            }
         }
-
-        Random rnd = new Random(RANDOM_SEED);
-        Tree a;
-        Image img;
-
-        if(choosedStrategy == 0){
-            a = Tree.generateRandomtree(nbLeaves, proportionCut, minDimensionCut, sameColorProb, widthLine, rnd, WIDTH, HEIGHT);
-            img=a.toImage();
-
-            try {
-                img.save("img.png");
-            } catch (Exception e) {
-                // handle exception
-                e.printStackTrace();
-            }
-        }else if(choosedStrategy == 1){
-            System.out.println("test");
-            a = Tree.generateBetterRandomtree(nbLeaves, proportionCut, minDimensionCut, sameColorProb, widthLine, rnd, WIDTH, HEIGHT, new Point(centerX, centerY));
-            img=a.toImage();
-
-            try {
-                img.save("img1.png");
-            } catch (Exception e) {
-                // handle exception
-                e.printStackTrace();
-            }
-        }else if(choosedStrategy == 2){
-
-            a = Tree.generateBetterRandomtree2(nbLeaves, proportionCut, minDimensionCut, sameColorProb, widthLine, rnd, WIDTH, HEIGHT, new Point(centerX, centerY));
-            img=a.toImage();
-
-            try {
-                img.save("img2.png");
-            } catch (Exception e) {
-                // handle exception
-                e.printStackTrace();
-            }
-        }
-        
-        
-
-
     }
 
     public static void setnbLeaves(int val) throws Exception{
